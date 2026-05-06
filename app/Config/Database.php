2313c78aa2721162527abe5f195c805d86772f52
AbhidevBaiju <abhideveb05@gmail.com>
2026-05-06 10:05:02 +0530

    railwayready

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'mysql.railway.internal',
+        'username'     => 'root',
+        'password'     => 'umIJHauSlkHMXKISPpGxQjxNIpRNyYTK',
+        'database'     => 'railway',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
